9f0681daf62ada006c9036a6c8c51c4de01682db
Jeyanthi Jegadeesh <jeyanthi.s.jegadeesh@gmail.com>
2024-04-18 22:11:30 +0200

    removed console log statements

--- server/service/spoonacularApiService.js
+++ server/service/spoonacularApiService.js
@@ -2,7 +2,6 @@ const {spoonacularBaseURL, apiKey} = require('../config.js');
 
 async function getRandomRecipes(){
     try {
-        console.log('API URL :::: ',`${spoonacularBaseURL}/recipes/random?number=10&apiKey=${apiKey}`);
         return await fetch(`${spoonacularBaseURL}/recipes/random?number=10&apiKey=${apiKey}`);
       } catch (error) {
         console.error('Error fetching recipe from Spoonacular api:', error);

48272b76d9b7f41166ae620393869a2d737b861e
Jeyanthi Jegadeesh <jeyanthi.s.jegadeesh@gmail.com>
2024-04-19 12:01:17 +0200

    feat: new angular app created in client folder

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync('noop')
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
